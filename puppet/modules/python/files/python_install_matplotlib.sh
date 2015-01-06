56b59453f1fd72d03bdd54c5aecfdb37e96a3d63
Philippe Breucker <breucker@ifris.org>
2015-01-06 18:23:34 +0100

    new install method merged in master branch.

--- puppet/modules/python/files/python_install_matplotlib.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#install matplotlib
-cd /home/vagrant
-git clone git://github.com/matplotlib/matplotlib.git
-cd matplotlib
-python setup.py install
--- puppet/test-puppet.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-##Testing all puppet manifests with puppet-lint (https://github.com/rodjek/puppet-lint)
-find -name '*.pp' | xargs -n 1 -t puppet parser validate
-find -name '*.pp' | xargs -n 1 -t puppet-lint --error-level error
-
