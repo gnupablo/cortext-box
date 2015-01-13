6bda638de00171efbb3d96deeb8dff7d1293b56f
Guillaume Orsal <guillaume@orsal.net>
2015-01-13 11:19:59 +0100

    Modification of upload max filesize

--- 2_install_MV.sh
+++ 2_install_MV.sh
@@ -1,5 +1,12 @@
 #/bin/sh
 
+# Apres la creation de la machine virtuelle, ce script realise l'installation et 
+# la configuration du systeme pour que les differents modules cortext puissent
+# s'executer, puis realise l'installation de ces modules
+
+
+# Verification que l'execution se fait bien en etant root et pas depuis un sudo
+
 if [ `id -u -n` != "root" ]
 then
    echo -e "\e[1;31mVous devez être root pour executer cette application\e[0m"
@@ -12,16 +19,26 @@ fi
 
 echo -e "\e[1;32m=== Script d'installation de la machine virtuelle\e[0m"
 
-# A executer avec les droits root sur la machine virtuelle une fois qu'elle aura été initialisée
+
+# Mise a jour du systeme
 
 echo -e "\e[1;32m=== Mise à jour de la machine virtuelle\e[0m"
 apt-get update
 apt-get -y --force-yes upgrade
 
+
+# Chargement de la liste des packages a installer et de leurs parametres
+
 debconf-set-selections /vagrant/preconfig.txt
+
+
+# Declenchement de l'installation des packages
 
 echo -e "\e[1;32m=== Installation des packages nécessaires à Cortext\e[0m"
 apt-get install -y --force-yes apache2 php5 php5-mysql mysql-server phpmyadmin libapache2-mod-wsgi libapache2-mod-php5 mysql-client php-pear curl php5-cli php5-dev php5-gd php5-curl php5-intl git postfix mailutils
+
+
+# Configuration du serveur Web Apache
 
 echo -e "\e[1;32m=== Activation des modules apache\e[0m"
 a2enmod rewrite headers
@@ -38,8 +55,15 @@ ln -s /etc/phpmyadmin/apache.conf /etc/apache2/conf.d/phpmyadmin.conf
 echo -e "\e[1;32m=== Modification du umask Apache\e[0m"
 echo umask 0002 >> /etc/apache2/envvars
 
+echo -e "\e[1;32m=== Augmentation de la taille maximal d'upload\e[0m"
+sed -i 's/upload_max_filesize = 2M/upload_max_filesize = 200M/g' /etc/php5/apache2/php.ini
+sed -i 's/upload_max_filesize = 2M/upload_max_filesize = 200M/g' /etc/php5/cli/php.ini
+
 echo -e "\e[1;32m=== Redémarrage d'Apache pour prendre en compte la nouvelle config\e[0m"
 service apache2 restart
+
+
+# Configuration du serveur mail Postfix pour qu'il traite les mails locaux
 
 echo -e "\e[1;32m=== Configuration de postfix\e[0m"
 sed -i 's/debian-7.7.0-amd64/cortext.dev/' /etc/postfix/main.cf
@@ -48,12 +72,18 @@ echo "default_transport = error
 relay_transport = error" >> /etc/postfix/main.cf
 service postfix restart
 
+
+# Installation de Composer necessaire pour les installations des modules
+
 echo -e "\e[1;32m=== Téléchargement de Composer\e[0m"
 cd /home/vagrant
 curl -s https://getcomposer.org/installer | php
 
 echo -e "\e[1;32m=== Création d'un lien vers Composer\e[0m"
 ln -s /home/vagrant/composer.phar /usr/local/bin/composer
+
+
+# Creation du script bash_aliases pour disposer immediatemment des alias
 
 echo -e "\e[1;32m=== Création des alias\e[0m"
 echo "export LS_OPTIONS='--color=auto'" >> /home/vagrant/.bash_aliases
@@ -72,12 +102,18 @@ echo "alias l='ls $LS_OPTIONS -l'" >> /root/.bashrc
 
 . /home/vagrant/.bash_aliases
 
+
+# Lancement de l'installation des modules Cortext
+
 echo -e "\e[1;32m=== Appels des scripts d'installation des modules Cortext\e[0m"
 cd /vagrant
 . 3_install_Auth.sh
 . 4_install_Assets.sh
 . 5_install_Manager.sh
 . add_etc_hosts.sh
+
+
+# Fin de l'installation, suggestion de l'etape suivante
 
 echo -e "\e[1;32m=== Fin, vous pouvez maintenant ajouter des données de test avec la commande suivante:\e[0m"
 echo -e "\e[1;31m./6_dummy_data.sh\e[0m"
