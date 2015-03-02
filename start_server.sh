a3e82d9307341829e9c44a52aa9e3f44645be3e0
Guillaume Orsal <guillaume@orsal.net>
2015-03-02 18:09:51 +0100

    New log directory for supervisor

--- start_server.sh
+++ start_server.sh
@@ -3,4 +3,4 @@
 # Script exécuté au démarrage de la machine virtuelle ou physique
 # Permet de lancer les serveurs qui ne sont pas lancés automatiquement
 
-supervisord -u vagrant
+supervisord -u vagrant -q /var/log/supervisor
