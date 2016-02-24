68805817ecfd4bea69913127bba72e5e76a94da4
Philippe Breucker <breucker@ifris.org>
2016-02-24 15:42:55 +0100

    vagrant package script

--- vagrant-package.sh
+++ vagrant-package.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-rm cortext.box
-vagrant package default virtualbox --output cortext.box
-scp cortext.box admifris@srvifris:/srv/local/web/webroot/public/upload/web/files/
+#rm cortext.box
+vagrant package default virtualbox --output cortext-jessie-docker-$1.box
+scp cortext-jessie-docker-$1.box admifris@srvifris:/srv/local/web/webroot/public/upload/web/files/
