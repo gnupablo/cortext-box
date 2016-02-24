7c2f6f3c5f214cb6d5edfeab5ebf2c55539de0e7
Guillaume Orsal <guillaume@orsal.net>
2016-02-24 16:15:30 +0100

    Nouvel emplacement de sauvegarde des box

--- vagrant-package.sh
+++ vagrant-package.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
-#rm cortext.box
 vagrant package default virtualbox --output cortext-jessie-docker-$1.box
-scp cortext-jessie-docker-$1.box admifris@srvifris:/srv/local/web/webroot/public/upload/web/files/
+scp cortext-jessie-docker-$1.box adminlisis@lisis-srv-prod.u-pem.fr:/srv/local/web/file.inra-ifris.org/files/
