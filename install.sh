bb0f8b43fd6c2d3704894130f89b2e86d9a49456
Guillaume Orsal <guillaume@orsal.net>
2016-02-29 18:51:56 +0100

    Ajout de mise en forme pour plus de lisibilité

--- install.sh
+++ install.sh
@@ -1,13 +1,25 @@
 #!/bin/sh
 
 tput setab 7; tput setaf 1;echo "3. Récupération des sources depuis GitHub$(tput sgr 0)"
+
+tput setab 7; tput setaf 1;echo "3.1 Cortext-auth$(tput sgr 0)"
 git clone --recursive git@github.com:cortext/cortext-auth.git
+
+tput setab 7; tput setaf 1;echo "3.2 Silex-simpleuser$(tput sgr 0)"
 cd cortext-auth/server/vendor/cortext/silex-simpleuser
 git checkout master
 cd ../../../../..
+
+tput setab 7; tput setaf 1;echo "3.3 Cortext-assets$(tput sgr 0)"
 git clone --recursive git@github.com:cortext/cortext-assets.git
+
+tput setab 7; tput setaf 1;echo "3.4 Cortext-manager$(tput sgr 0)"
 git clone --recursive git@github.com:cortext/cortext-manager.git
+
+tput setab 7; tput setaf 1;echo "3.5 Cortext-projects$(tput sgr 0)"
 git clone --recursive git@github.com:cortext/cortext-projects.git
+
+tput setab 7; tput setaf 1;echo "3.6 Cortext-methods$(tput sgr 0)"
 git clone --recursive git@github.com:cortext/cortext-methods.git
 
 tput setab 7; tput setaf 1;echo "4. Téléchargement de la machine virtuelle$(tput sgr 0)"
