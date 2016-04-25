4eab633165673eb0d2d287928b8d0748a97036c6
Guillaume Orsal <guillaume@orsal.net>
2016-04-25 12:24:29 +0200

    Version RISIS de cortext-box

--- install.sh
+++ install.sh
@@ -3,7 +3,7 @@
 tput setab 7; tput setaf 1;echo "3. Récupération des sources depuis GitHub$(tput sgr 0)"
 
 tput setab 7; tput setaf 1;echo "3.1 Cortext-auth$(tput sgr 0)"
-git clone --recursive git@github.com:cortext/cortext-auth.git
+git clone -b risis_v1 --recursive git@github.com:cortext/cortext-auth.git
 
 tput setab 7; tput setaf 1;echo "3.2 Silex-simpleuser$(tput sgr 0)"
 cd cortext-auth/server/vendor/cortext/silex-simpleuser
@@ -11,13 +11,13 @@ git checkout master
 cd ../../../../..
 
 tput setab 7; tput setaf 1;echo "3.3 Cortext-assets$(tput sgr 0)"
-git clone --recursive git@github.com:cortext/cortext-assets.git
+git clone -b risis_v1 --recursive git@github.com:cortext/cortext-assets.git
 
 tput setab 7; tput setaf 1;echo "3.4 Cortext-manager$(tput sgr 0)"
-git clone --recursive git@github.com:cortext/cortext-manager.git
+git clone -b risis_v1 --recursive git@github.com:cortext/cortext-manager.git
 
 tput setab 7; tput setaf 1;echo "3.5 Cortext-projects$(tput sgr 0)"
-git clone --recursive git@github.com:cortext/cortext-projects.git
+git clone -b risis_v1 --recursive git@github.com:cortext/cortext-projects.git
 
 tput setab 7; tput setaf 1;echo "3.6 Cortext-methods$(tput sgr 0)"
 git clone --recursive git@github.com:cortext/cortext-methods.git
