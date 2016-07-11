4142659d3b0c2b2f05032d17e5b546931c142581
Guillaume Orsal <guillaume@orsal.net>
2016-07-11 15:46:03 +0200

    Fichier d'installation pour windows

---
+++ install_win.bat
@@ -0,0 +1,27 @@
+echo "3. Récupération des sources depuis GitHub"
+
+echo "3.1 Cortext-auth"
+git clone --recursive git@github.com:cortext/cortext-auth.git
+
+echo "3.2 Silex-simpleuser"
+cd cortext-auth/server/vendor/cortext/silex-simpleuser
+git checkout master
+cd ../../../../..
+
+echo "3.3 Cortext-assets"
+git clone --recursive git@github.com:cortext/cortext-assets.git
+
+echo "3.4 Cortext-manager"
+git clone --recursive git@github.com:cortext/cortext-manager.git
+
+echo "3.5 Cortext-projects"
+git clone --recursive git@github.com:cortext/cortext-projects.git
+
+echo "3.6 Cortext-methods"
+git clone --recursive git@github.com:cortext/cortext-methods.git
+
+echo "4. Téléchargement de la machine virtuelle"
+echo "Une fois la machine virtuelle démarrée, lancer la suite de l'exécution par la commande suivante:"
+echo "install_inside.sh"
+vagrant up
+vagrant ssh
