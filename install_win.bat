256d54393db3c872f84e7dd969cfceffbf401bc4
Guillaume Orsal <guillaume@orsal.net>
2016-07-12 10:24:58 +0200

    Amélioration de la lisisibilité

--- install_win.bat
+++ install_win.bat
@@ -1,30 +1,38 @@
 @echo off
-echo 3. Recuperation des sources depuis GitHub
+echo
+echo ######## 3. Recuperation des sources depuis GitHub ########
 
-echo 3.1 Cortext-auth
+echo ######## 3.1 Cortext-auth ########
 git clone --recursive git@github.com:cortext/cortext-auth.git
 
-echo 3.2 Silex-simpleuser
+echo
+echo ######## 3.2 Silex-simpleuser ########
 cd cortext-auth/server/vendor/cortext/silex-simpleuser
 git checkout master
 cd ../../../../..
 
-echo 3.3 Cortext-assets
+echo
+echo ######## 3.3 Cortext-assets ########
 git clone --recursive git@github.com:cortext/cortext-assets.git
 
-echo 3.4 Cortext-manager
+echo
+echo ######## 3.4 Cortext-manager ########
 git clone --recursive git@github.com:cortext/cortext-manager.git
 
-echo 3.5 Cortext-projects
+echo
+echo ######## 3.5 Cortext-projects ########
 git clone --recursive git@github.com:cortext/cortext-projects.git
 
-echo 3.6 Cortext-methods
+echo
+echo ######## 3.6 Cortext-methods ########
 git clone --recursive git@github.com:cortext/cortext-methods.git
 
-echo 3.8 Identification de l'installation en environnement Windows
+echo
+echo ######## 3.7 Identification de l'installation en environnement Windows ########
 echo. > win_install.flag
 
-echo 4. Telechargement de la machine virtuelle
+echo
+echo ######## 4. Telechargement de la machine virtuelle ########
 echo Une fois la machine virtuelle demarree, lancer la suite de l'execution par la commande suivante:
 echo install_inside.sh
 vagrant up
