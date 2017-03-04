69f3e943ef2a2b352cebf80646008d7814ce55c5
Guillaume Orsal <guillaume@orsal.net>
2017-03-04 08:48:44 +0100

    Gestion d'un suffixe en paramètre, sinon horodatage + commentaires

--- vagrant-package.sh
+++ vagrant-package.sh
@@ -1,3 +1,16 @@
 #!/bin/sh
-vagrant package default virtualbox --output cortext-jessie-docker-$1.box
-scp cortext-jessie-docker-$1.box adminlisis@lisis-srv-prod.u-pem.fr:/srv/local/web/file.inra-ifris.org/files/
+# Script de génération d'un package contenant l'état actuel de la box, plus push sur le serveur de fichier Cortext
+
+# Gestion du suffixe de l'archive, si un paramètre est passé au script, il remplace le timestamp
+if [ "$1" == "" ]
+then
+  suff=`date +%Y%m%d-%H%M%S`
+else
+  suff=$1
+fi
+
+# Génération du package
+vagrant package default virtualbox --output cortext-jessie-docker-$suff.box
+
+# Push du package sur le serveur de stockage
+scp cortext-jessie-docker-$suff.box adminlisis@lisis-srv-prod.u-pem.fr:/srv/local/web/file.inra-ifris.org/files/
